2dcd1018bb7a1df18deabbad905a219d03f1e3ba
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-18 21:44:08 +0100

    Exercise 1 completed

---
+++ 1-Template_Literals/index.js
@@ -0,0 +1,17 @@
+let word1 = "Amy";
+let word2 = "Richards"
+let num1 = 9;
+let num2 = 3;
+
+// const fullName = word1 + ' ' + word2;
+const fullName = `${word1} ${word2}`;
+console.log(fullName);
+
+
+// const addNumbers = num1 + num2
+const addNumbers = `${num1 + num2}`;
+console.log(addNumbers);
+
+//let example = "Amy \n" + "Richards";
+let example = `${word1}
+${word2}`;
