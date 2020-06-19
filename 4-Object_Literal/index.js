343c3ac6c63b4c10e1a6881e8c215738a4ca911e
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-19 12:09:56 +0100

    exercise 4 completed

--- 4-Object_Literal/index.js
+++ 4-Object_Literal/index.js
@@ -1,5 +1,7 @@
 function addressMaker(city, state) {
-
+    //const newAddress = {newCity: city, newState: state};
+    const newAddress = {city, state};
+    console.log(newAddress);
 }
 
 addressMaker("Austin", "Texas");
