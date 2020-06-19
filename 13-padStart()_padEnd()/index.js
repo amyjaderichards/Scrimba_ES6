43026ec7f23a9842cbaa2e64bee39cb960d62946
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-19 13:45:14 +0100

    Exercise completed

---
+++ 13-padStart()_padEnd()/index.js
@@ -0,0 +1,12 @@
+let example = 'Amy';
+console.log(example.padStart(10, 'a'));
+// aaaaaaaAmy
+
+
+let example2 = 'Amy Richards';
+console.log(example2.padStart(10, 'a'));
+// Amy Richards
+
+let example3 = 'Amy'
+console.log(example3.padEnd(10, 'a'));
+// Amyaaaaaaa
