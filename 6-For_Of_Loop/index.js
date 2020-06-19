5ac752c96c5529031d411d1a3dc7b7a262502948
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-19 12:30:39 +0100

    Exercise 6 completed

---
+++ 6-For_Of_Loop/index.js
@@ -0,0 +1,13 @@
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+    total += income;
+}
+
+
+let fullName = "Amy Jade Richards"
+
+for (const char of fullName) {
+    console.log(char);
+}
